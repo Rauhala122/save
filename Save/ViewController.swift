6367d39f12cb78e579a285dd483a27b38cb302d0
Saska Rauhala <saskarauhala@saska-macbook-pro.local>
2017-01-28 17:37:09 +0200

    Initial Commit

---
+++ Save/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Save
+//
+//  Created by Saska Rauhala on 28.1.2017.
+//  Copyright © 2017 Rauhala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
